b39d23141a56a5e0d29754172c59b60ea1e5c88b
linkermi <50197678+linkermi@users.noreply.github.com>
2019-05-12 15:05:37 +0200

    Bla

--- views/view.home.php
+++ views/view.home.php
@@ -1,5 +1,5 @@
-Willkommen beim Wingduell
+<h1> Willkommen beim Wingduell</h1>
 
-        <a href="?task=login" data-ajax="false" data-role="button">Anmelden</a>
+<a href="?task=login" data-ajax="false" data-role="button"><p>Anmelden</p></a>
 
         <a href="?task=registrieren" data-ajax="false" data-role="button">Neu registrieren</a>
