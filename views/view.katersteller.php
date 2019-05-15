3c7086ec753432c4c707e19e23b1d0cc8817f336
linkermi <50197678+linkermi@users.noreply.github.com>
2019-05-15 17:26:48 +0200

    Button änderung

--- views/view.katersteller.php
+++ views/view.katersteller.php
@@ -9,8 +9,8 @@ $Teilnehmer=Core::$view->Teilnehmer;
             <div><img style="border-radius: 2em;margin:0;padding-top: 1px;" src="includes/images/<?=$Teilnehmer->avatar?>" alt="avatar" width="38" height="38"/> <?=$Teilnehmer->nickname.'('.$Teilnehmer->rating.')'?></div>
 
             <input type="hidden" id="id"  name="id" value="<?=$Teilnehmer->m_oid?>" />
-		 <?php Help::htmlRadioGroup($kategorien,array("type"=>"buttonmini", "label"=>"Kategorie 1","name"=>"Kategorie1"))?>
-		 <?php Help::htmlRadioGroup($kategorien,array("type"=>"buttonmini", "label"=>"Kategorie 2","name"=>"Kategorie2"))?>
+		 <?php Help::htmlSelect($kategorien,array("type"=>"buttonmini", "label"=>"Kategorie 1","name"=>"Kategorie1"))?>
+		 <?php Help::htmlSelect($kategorien,array("type"=>"buttonmini", "label"=>"Kategorie 2","name"=>"Kategorie2"))?>
             <br>
               <button type="submit" name="create" id="create" value="1">Herausfordern</button>
         </div>
