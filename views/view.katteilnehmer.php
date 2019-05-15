fca533f1e75c9935b9a7771f687ebb6a3b470ea6
linkermi <50197678+linkermi@users.noreply.github.com>
2019-05-15 17:27:00 +0200

    Button änderung

--- views/view.katteilnehmer.php
+++ views/view.katteilnehmer.php
@@ -9,8 +9,8 @@ $a=1;
 	<div class="ui-field-contain">
              <div><img style="border-radius: 2em;margin:0;padding-top: 1px;" src="includes/images/<?=$Teilnehmer->avatar?>" alt="avatar" width="38" height="38"/> <?=$Teilnehmer->nickname.'('.$Teilnehmer->rating.')'?></div>
             <input type="hidden" id="id"  name="id" value="<?= Core::$view->id?>" />
-		 <?php Help::htmlRadioGroup($kategorien,array("type"=>"buttonmini", "label"=>"Kategorie 1","name"=>"Kategorie1"))?>
-		 <?php Help::htmlRadioGroup($kategorien,array("type"=>"buttonmini", "label"=>"Kategorie 2","name"=>"Kategorie2"))?>
+		 <?php Help::htmlSelect($kategorien,array("type"=>"buttonmini", "label"=>"Kategorie 1","name"=>"Kategorie1"))?>
+		 <?php Help::htmlSelect($kategorien,array("type"=>"buttonmini", "label"=>"Kategorie 2","name"=>"Kategorie2"))?>
               <button type="submit" name="create" id="create" value="1">los geht's</button>
         </div>
 </form>
