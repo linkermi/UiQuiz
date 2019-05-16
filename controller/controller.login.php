23a7e12dec531c8de31158b685fad28784de0ba6
linkermi <50197678+linkermi@users.noreply.github.com>
2019-05-16 17:03:43 +0200

    Animation login

--- controller/controller.login.php
+++ controller/controller.login.php
@@ -1,5 +1,5 @@
 <?php
-Core::$view->path["view1"]="views/view.login.php";
+Core::$view->path["view1"]="views/view.login2.php";
 if(count($_POST)>0){
  //Core::$user=new User();
   Core::$user->login(filter_input(INPUT_POST,"username"), filter_input(INPUT_POST, "passwort"));  
