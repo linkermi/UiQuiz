6c215d8987c2439c22398d290d82c3b657336b91
riedelfa <riedelfa@WingIT-10.fhrz.ad.fh-pforzheim.de>
2019-05-18 12:56:08 +0200

    animation startseite

--- views/view.home.php
+++ views/view.home.php
@@ -1,5 +1,18 @@
-<h1> Willkommen beim Wingduell</h1>
+<div id="aaa" style="z-index: 4;opacity: 0.5;position: absolute"> 
+
+<h1> Willkommen beim Wingduell </h1>
 
 <a href="?task=login" data-ajax="false" data-role="button"><p>Anmelden</p></a>
 
-<a href="?task=registrieren" data-ajax="false" data-role="button"><p>Neu registrieren</p></a>
+<a href="?task=registrieren" data-ajax="false" data-role="button"><p>Neu registrieren</p></a>   
+</div>
+
+
+<div style="width: 100%;height: 100%;position: absolute;margin-left: 0px;margin-top: 0px">
+    
+<iframe src="views/view.animation.php" width="100%" height="100%" style="z-index: 3"></iframe>
+</div>
+
+
+
+
