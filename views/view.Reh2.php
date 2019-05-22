e8c5301b1da4ca26961715c2ca200f08b3b2494a
riedelfa <riedelfa@WingIT-10.fhrz.ad.fh-pforzheim.de>
2019-05-22 13:48:30 +0200

    Test View für die Reh/Hase Animation

---
+++ views/view.Reh2.php
@@ -0,0 +1,12 @@
+<h5>Where am I ? 
+        <style>
+            h5::after{content: "+";
+                float: right;
+                margin-right: 5px;}
+        </style>
+    </h5>
+
+<div style="width: 100%;height: 100%;position: absolute;margin-left: 0px;margin-top: 0px">
+    
+<iframe src="views/view.Reh.php" width="100%" height="100%" style="z-index: 3"></iframe>
+</div>
