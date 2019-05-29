aeced457fea9f68a1d7f1e9d04d07aae18a97d85
riedelfa <riedelfa@WingIT-10.fhrz.ad.fh-pforzheim.de>
2019-05-29 17:03:45 +0200

    Hintergrundmusik: iframe ; not working :(

---
+++ views/youtube.php
@@ -0,0 +1,7 @@
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/XUWnrhKTu9M&autoplay=1&mute=1"
+        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allow="autoplay"
+        allowfullscreen>
+            
+</iframe> 
